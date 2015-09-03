e6d3eaedc1ab2dd52a71a62248d26a841bbee687
kevm254 <kevm2@yahoo.com>
2015-09-03 08:35:43 -0500

    Added password authentication to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false }
+  validates :password, length: { minimum: 7 }
+  has_secure_password
 end

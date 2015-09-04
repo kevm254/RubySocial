7d3adbbeab6ed5b4bc0d5b8251ab019790520b7c
kevm254 <kevm2@yahoo.com>
2015-09-04 11:01:46 -0500

    login options

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ group :development, :test do
   gem 'spring'
 end
 
+group :production do
+  gem 'puma', '2.11.1'
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  protect_from_forgery with: :exception
+
   def home
   end
 
@@ -7,8 +10,7 @@ class ApplicationController < ActionController::Base
 
   def about
   end
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end

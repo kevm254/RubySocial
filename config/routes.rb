396546c8bf78ecdd3c3db4309e3c44adff924da8
kevm254 <kevm2@yahoo.com>
2015-09-04 02:42:35 -0500

    more tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
-  get 'signup' => 'users#new'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  root   'static_pages#home'
+  get    'signup'   => 'users#new'
+  get    'help'     => 'static_pages#help'
+  get    'about'    => 'static_pages#about'
+  get    'contact'  => 'static_pages#contact'
+  resources :users
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "superduper", password_confirmation: "superduper")
   end
 
   test "should be valid" do
-    assert @user.valid?
+    assert @user.valid?, "user is valid"
   end
 
   test "name should be present" do
@@ -34,7 +34,7 @@ class UserTest < ActiveSupport::TestCase
     valid_addresses = %w[user@example.com USER@foo.COM A_USER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert @user.valid?, "# {valid_address.inspect} should be valid"
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 

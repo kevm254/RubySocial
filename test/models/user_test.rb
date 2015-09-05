3ab2a1780c876fe825d0b9221382eae3b493bcef
kevm254 <kevm2@yahoo.com>
2015-09-04 20:55:13 -0500

    Finish login system

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,7 +47,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
@@ -58,5 +57,9 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end

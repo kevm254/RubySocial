9bc510365a7852a26e629be6e9b9d09a01128d01
kevm254 <kevm2@yahoo.com>
2015-09-05 16:05:03 -0500

    login adjustments

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { name: '', email: 'foo@invalid', password: 'what', password_confirmation: 'when'  }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Youji Yamson"
+    email = "secret@mindyabiz.com"
+    patch user_path(@user), user: { name: name, email: email, password: "", password_confirmation: "" }
+    assert_not flash.empty?
+    @user.reload
+    assert_redirected_to @user
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+end

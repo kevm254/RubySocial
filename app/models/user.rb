1527a70364bf039a5da46b242dbbabb749bf3007
kevm254 <kevm2@yahoo.com>
2015-09-11 15:35:47 -0500

    Status feed completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,6 +99,25 @@ class User < ActiveRecord::Base
     following.include?(other_user)
   end
 #===========================================================================#
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+#===========================================================================#
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+#===========================================================================#
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+
+
+#===========================================================================#
 #===========================================================================#
   private
   # Converts email to all lowercase.
